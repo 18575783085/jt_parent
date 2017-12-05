b24470aeb003150e1cc4953019becc3caeec1747
oye <517553812@qq.com>
2017-12-06 00:23:38 +0800

    创建了oye分支，重新提交到GitHub

--- jt_manage_mapper/src/main/java/top/ou/jt/manage/mapper/ItemCatMapper.java
+++ jt_manage_mapper/src/main/java/top/ou/jt/manage/mapper/ItemCatMapper.java
@@ -3,6 +3,9 @@ package top.ou.jt.manage.mapper;
 import top.ou.jt.common.mapper.SysMapper;
 import top.ou.jt.manage.pojo.ItemCat;
 
+/**
+ * 通用性Mapper
+ */
 public interface ItemCatMapper extends SysMapper<ItemCat>{
 	
 
