705d69b9e3e97a6582d1e7bfc365e996e787f15c
oye <517553812@qq.com>
2017-12-08 00:53:47 +0800

    完成需求：1.分页查询商品；2.新增、修改、批量删除商品

--- jt_manage_controller/src/main/java/top.ou/jt/manage/controller/ItemController.java
+++ jt_manage_controller/src/main/java/top.ou/jt/manage/controller/ItemController.java
@@ -10,11 +10,15 @@
  */
 package top.ou.jt.manage.controller;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import top.ou.jt.common.vo.EasyUIResult;
+import top.ou.jt.common.vo.SysResult;
 import top.ou.jt.manage.pojo.Item;
 import top.ou.jt.manage.service.ItemService;
 
@@ -35,10 +39,72 @@ public class ItemController {
     @Autowired
     private ItemService itemService;
 
+    @Autowired
+    private static final Logger log = Logger.getLogger(ItemController.class);
+
     @RequestMapping("/query")
     @ResponseBody
-    public EasyUIResult queryItemList(){
+    public EasyUIResult queryItemList(Integer page,Integer rows){
+        PageHelper.startPage(page,rows);
         List<Item> itemList = itemService.queryItemList();
-        return new EasyUIResult(itemList.size(),itemList);
+        PageInfo<Item> pageInfo = new PageInfo<Item>(itemList);
+        return new EasyUIResult(pageInfo.getTotal(),pageInfo.getList());
+    }
+
+    /**
+     * 新增商品
+     * @param item 商品实体
+     * @return
+     */
+    @RequestMapping("/save")
+    @ResponseBody
+    public SysResult saveItem(Item item){
+        try {
+            itemService.saveItem(item);
+            return SysResult.oK();
+        }catch (Exception e){
+            //写出错误日志
+            String message = e.getMessage();
+            log.error(message);
+            //返回错误信息
+            return  SysResult.build(201,"新增商品："+message);
+        }
+    }
+
+    /**
+     * 修改商品
+     * @param item 商品实体
+     * @return
+     */
+    @RequestMapping("/update")
+    @ResponseBody
+    public SysResult updateItem(Item item){
+        try{
+            itemService.updateItem(item);
+            return SysResult.oK();
+        }catch (Exception e){
+            //写出错误日志
+            String msg = e.getMessage();
+            log.error(msg);
+            return SysResult.build(201,msg);
+        }
+    }
+
+    /**
+     * 单个或批量删除商品
+     * @param ids 商品id
+     * @return
+     */
+    @RequestMapping("/delete")
+    @ResponseBody
+    public SysResult deleteItems(Long[] ids){
+        try{
+            itemService.deleteItems(ids);
+            return SysResult.oK();
+        }catch (Exception e){
+            //写出错误日志
+            log.error(e.getMessage());
+            return SysResult.build(201,e.getMessage());
+        }
     }
 }
--- jt_manage_service/src/main/java/top/ou/jt/manage/service/ItemService.java
+++ jt_manage_service/src/main/java/top/ou/jt/manage/service/ItemService.java
@@ -15,6 +15,7 @@ import org.springframework.stereotype.Service;
 import top.ou.jt.manage.mapper.ItemMapper;
 import top.ou.jt.manage.pojo.Item;
 
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -34,5 +35,38 @@ public class ItemService extends BaseService<Item>{
     public List<Item> queryItemList() {
         List<Item> itemList = itemMapper.queryItemList();
         return itemList;
+    }
+
+
+    /**
+     * 新增商品
+     * @param item
+     */
+    public void saveItem(Item item) {
+        //设置商品状态 1-上架 2-删除
+        item.setStatus(1);
+        //设置商品上架时间
+        item.setCreated(new Date());
+        //设置商品更新时间
+        item.setUpdated(new Date());
+        itemMapper.insertSelective(item);
+    }
+
+    /**
+     * 修改商品信息
+     * @param item
+     */
+    public void updateItem(Item item) {
+        //修改商品更新时间
+        item.setUpdated(new Date());
+        itemMapper.updateByPrimaryKeySelective(item);
+    }
+
+    /**
+     * 单个或批量删除商品
+     * @param ids
+     */
+    public void deleteItems(Long[] ids) {
+        itemMapper.deleteByIDS(ids);
     }
 }
